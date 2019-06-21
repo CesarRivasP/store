571be1f96c3e215ba8e9c48c5bc274254ce473b2
CesarRivasP <cesarjose58@gmail.com>
2019-06-21 19:52:06 -0400

    Create and implemented single-product component

---
+++ client/src/components/single-product/single-product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './single-product.css';
+
+function SingleProduct({product}){
+  const { image, name, price, description } = product;
+
+  if(!product) return null;
+
+  return (
+    <div className="info-product">
+      <div className="image">
+        <img src={`/img/${image}.png`} alt={name}/>
+      </div>
+
+      <div className="info">
+        <h2>{name}</h2>
+        <p className="price">$ {price}</p>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SingleProduct;
