8bec4c7cfdb070e90b2d85e915a840d72de994c5
CesarRivasP <cesarjose58@gmail.com>
2019-06-21 19:50:04 -0400

    IMplemented router in app.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Router from './components/router';
+
+
+function App() {
+  return (
+    <Fragment>
+      <Router />
+    </Fragment>
+  );
+}
+
+export default App;
