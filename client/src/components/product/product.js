72974caa714a913a9eae44a985a8cb6f78ebc012
CesarRivasP <cesarjose58@gmail.com>
2019-06-21 19:51:37 -0400

    Create and implemented product component

---
+++ client/src/components/product/product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './list-prod.css';
+import { Link } from 'react-router-dom';
+
+function Product({information}){
+  const { image, name, price, id } = information;
+
+  return (
+    <li className="list-prod">
+      <img src={`/img/${image}.png`} alt={name}/>
+      <p>{ name } <span>$ {price}</span></p>
+      <Link to={`/producto/${id}`}>Más Información</Link>
+    </li>
+  );
+}
+
+export default Product;
