8b9ad79f7b88c8e81e54d64403be0c597a0779de
CesarRivasP <cesarjose58@gmail.com>
2019-06-21 19:52:13 -0400

    Create and implemented we component

---
+++ client/src/components/we/we.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './we.css';
+
+function We(){
+  return (
+    <div className="container-we">
+      <div className="image">
+        <img src="/img/camisa_1.png" alt="Imagen Nosotros" />
+      </div>
+      <div className="content">
+        <h2>Sobre Nosotros</h2>
+        <p>
+          Aenean auctor, augue id porta viverra, dui odio vehicula sem, eget imperdiet est tellus quis massa. Pellentesque tempor ac mauris id elementum. Donec luctus mauris at arcu semper, et luctus quam sagittis. Curabitur arcu lectus, porta et vulputate eget, ullamcorper non est. Praesent enim lectus, dictum ut scelerisque in, fringilla quis velit. Phasellus mattis ultricies sem, vitae egestas metus fermentum eget. Suspendisse pretium viverra magna, id tristique ipsum ornare ut.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default We;
