4e83f69b97a6d04ae86dda06e15f1c675563c485
CesarRivasP <cesarjose58@gmail.com>
2019-06-22 10:15:17 -0400

    IMplemented librarys and configuration

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import jwt from 'express-jwt';
+import cors from 'cors';
 import jwks from 'jwks-rsa';
-
+import jwtAuthz from 'express-jwt-authz';
+import bodyParser from 'body-parser';
+// crear servidor
 const app = express();
+
+// configurar para que utilice json y urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Para que tenga cors habilitado. Asi podra ser consumido desde otro servidor
+app.use(cors());
 
 const jwtCheck = jwt({
       secret: jwks.expressJwtSecret({
@@ -16,8 +25,11 @@ const jwtCheck = jwt({
     algorithms: ['RS256']
 });
 
-app.use(jwtCheck);
+// endpoints
+app.get('/productos', (request, response) => {
+  console.log(request);
+});
 
-app.get('/authorized', function (req, res) {
-    res.send('Secured Resource');
+app.listen(5000, () => {
+  console.log('Servidor corriendo');
 });
