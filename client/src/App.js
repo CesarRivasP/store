c4013e1c57f2e0ae8a0ce775e0b6ca1ecf86d5cc
CesarRivasP <cesarjose58@gmail.com>
2019-06-22 10:32:17 -0400

    implemented routes component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react';
 import Router from './components/router';
+import { makeMainRoutes } from './components/routes';
 
+const routes = makeMainRoutes();
 
 function App() {
   return (
     <Fragment>
-      <Router />
+      // <Router />
+      { routes }
     </Fragment>
   );
 }
