e550aaf75adce3a01b780d7ebe4aa35a3e0c8956
CesarRivasP <cesarjose58@gmail.com>
2019-06-23 08:00:31 -0400

    added apiURL atribute

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'cesarrivas10.auth0.com',
   clientID: '1d4VAPasHCzD35T11ElgITzaywNQlQUJ',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback',
+  apiURL: 'http://productos'
 }
