8a1f96d779a9a31f64b85256e4d099de47e59b69
CesarRivasP <cesarjose58@gmail.com>
2019-06-23 08:01:17 -0400

    Chnage to class component and added others functions

--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Product from '../product/product';
 import Search from '../search/search';
 import './products.css';
 
-const Products = ({ products, searchProduct }) => (
-  <div className="products">
-    <h2>Nuestros Productos</h2>
+class Products extends Component {
 
-    <Search search={searchProduct}/>
+  state = {
+    products: [],
+    searchTerm: ''
+  }
 
-    <ul className="products-list">
-      {
-        Object.keys(products).map((product) => (
-          <Product information={products[product]} key={product} />
-        ))
-      }
-    </ul>
-  </div>
-);
+  componentDidMount(){
+    this.queryAPI();
+  }
+
+  queryAPI = () => {
+    console.log(this.props.auth.isAuthenticated());
+  }
+
+  render(){
+    return (
+        <div className="products">
+          <h2>Nuestros Productos</h2>
+
+          <Search /*search={searchProduct}*/ />
+
+
+        </div>
+    );
+  }
+}
 
 export default Products;
+
+// render={(props) => {
+//   let id_product = props.location.pathname.replace('/producto/', '');
+//   return (
+//     <SingleProduct
+//       product={this.state.products[id_product]}
+//       key={id_product}
+//       auth={auth} {...props}
+//     />
+//   );
+// }}
+
+// ---- //
+// <ul className="products-list">
+//   // {
+//   //   Object.keys(products).map((product) => (
+//   //     <Product information={products[product]} key={product} />
+//   //   ))
+//   // }
+// </ul>
