efe8c0dc0a6f3275a6971c389747e34fbcb717d0
CesarRivasP <cesarjose58@gmail.com>
2019-06-23 17:44:58 -0400

    Change to class component and implemented state and others functions

--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 import Product from '../product/product';
 import Search from '../search/search';
 import './products.css';
@@ -11,30 +12,78 @@ class Products extends Component {
   }
 
   componentDidMount(){
+    // this.props.auth.logout();
+
     this.queryAPI();
   }
 
   queryAPI = () => {
-    console.log(this.props.auth.isAuthenticated());
+    const { getAccessToken } = this.props.auth;
+    // headers de autorizacion
+    const headers = { 'Authorization': `Bearer ${getAccessToken()}`}; //nos permite ganar autorizacion con un token valido
+    // En el servidor de express
+
+    // Que url se va a utilizar
+    const url = 'http://localhost:5000/productos';
+
+    return axios.get(url, { headers })
+      .then((response) => this.setState({
+        products: response.data
+      }, () => this.props.productsList(this.state.products)))
   }
 
   youArelogin = () => {
     this.props.auth.login();
   }
 
+  searchProduct = (search) => {
+    if(search.length > 3){ //si se tienen al menos 3 caracteres, realiza la busqueda, sino, hace otra llamado a la api para
+      // tener todos los productos
+
+      // obtener copia del state
+      const products = [...this.state.products];
+
+      // despues filtrar
+      let result;
+
+      result = products.filter((product) => (
+        product.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      ));
+
+      // enviar al state los productos filtrados y la busqueda
+      this.setState({
+        searchTerm: search,
+        products: result
+      })
+    }
+    else {
+      this.setState({
+        searchTerm: ''
+      }, () => {
+        this.queryAPI();
+      })
+    }
+  }
+
   render(){
     const { isAuthenticated } = this.props.auth;
+    const { products } = this.state;
     return (
       <div className="products">
-        <h2>Nuestros Productos</h2>
-
-        <Search /*search={searchProduct}*/ />
-
         {
           isAuthenticated() && (
-            <ul className="products-list">
-              <p>Estas logueado</p>
-            </ul>
+            <Fragment>
+              <h2>Nuestros Productos</h2>
+
+              <Search search={this.searchProduct} />
+              <ul className="products-list">
+                {
+                  Object.keys(products).map((product) => (
+                    <Product information={products[product]} key={product} />
+                  ))
+                }
+              </ul>
+            </Fragment>
           )
         }
 
@@ -43,7 +92,7 @@ class Products extends Component {
             !isAuthenticated() && (
               <div className="container-button">
                 <p>Para ver el contenido debes estar logueado</p>
-                <a onClick={this.youArelogin} className="button">Iniciar Sesión</a>
+                <a href="#!" onClick={this.youArelogin} className="button">Iniciar Sesión</a>
               </div>
             )
           }
@@ -55,23 +104,3 @@ class Products extends Component {
 }
 
 export default Products;
-
-// render={(props) => {
-//   let id_product = props.location.pathname.replace('/producto/', '');
-//   return (
-//     <SingleProduct
-//       product={this.state.products[id_product]}
-//       key={id_product}
-//       auth={auth} {...props}
-//     />
-//   );
-// }}
-
-// ---- //
-// <ul className="products-list">
-//   // {
-//   //   Object.keys(products).map((product) => (
-//   //     <Product information={products[product]} key={product} />
-//   //   ))
-//   // }
-// </ul>
