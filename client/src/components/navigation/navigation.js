5ca8bc1e44851bafcfd8e11a62ecc420df8e590d
CesarRivasP <cesarjose58@gmail.com>
2019-06-23 17:43:40 -0400

    Implement who class component and close and start session functions

--- client/src/components/navigation/navigation.js
+++ client/src/components/navigation/navigation.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navigation.css';
 
-const Navigation = () => (
-  <nav className="navigation">
-    <NavLink to={'/nosotros'} activeClassName="active">Nosotros</NavLink>
-    <NavLink to={'/productos'} activeClassName="active">Productos</NavLink>
-    <NavLink to={'/contacto'} activeClassName="active">Contacto</NavLink>
-  </nav>
-);
+class Navigation extends Component {
+
+  startSession = () => {
+    this.props.auth.login();
+  }
+
+  closeSession = () => {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    let result;
+    console.log(isAuthenticated());
+    if(isAuthenticated()){
+      result = <a href="#!" onClick={this.closeSession}>Cerrar Sesión</a>;
+    }
+    else {
+      result = <a href="#!" onClick={this.startSession}>Iniciar Sesión</a>;
+    }
+
+    return (
+      <nav className="navigation">
+        <NavLink to={'/nosotros'} activeClassName="active">Nosotros</NavLink>
+        <NavLink to={'/productos'} activeClassName="active">Productos</NavLink>
+        <NavLink to={'/contacto'} activeClassName="active">Contacto</NavLink>
+        { result }
+      </nav>
+    );
+  }
+}
 
 export default Navigation;
