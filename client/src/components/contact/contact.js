9647eab81bb2e112561e2ce619007202bfc563cf
CesarRivasP <cesarjose58@gmail.com>
2019-06-23 17:42:52 -0400

    Change to Class component and implemented conditional components

--- client/src/components/contact/contact.js
+++ client/src/components/contact/contact.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component, Fragment} from 'react';
 import './contact.css';
 
-const Contact = () => (
-  <form>
-    <legend>Formulario de Contacto</legend>
+class Contact extends Component {
 
-    <div className="input-field">
-      <label htmlFor="name">Name</label>
-      <input type="text" placeholder="name" id="name"/>
-    </div>
-    <div className="input-field">
-      <label htmlFor="email">Email</label>
-      <input type="email" placeholder="Tu email" id="email" />
-    </div>
-    <div className="input-field">
-      <label htmlFor="message">Mensaje</label>
-      <textarea name="message" id="message" cols="30" rows="10"></textarea>
-    </div>
-    <div className="input-field send">
-      <input type="submit" value="send"/>
-    </div>
-  </form>
-);
+  youArelogin = () => {
+    this.props.auth.login();
+  }
+
+  render(){
+    const { isAuthenticated } = this.props.auth;
+    return (
+      <Fragment>
+        {
+          isAuthenticated() && (
+            <form>
+              <legend>Formulario de Contacto</legend>
+
+              <div className="input-field">
+                <label htmlFor="name">Name</label>
+                <input type="text" placeholder="name" id="name"/>
+              </div>
+              <div className="input-field">
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder="Tu email" id="email" />
+              </div>
+              <div className="input-field">
+                <label htmlFor="message">Mensaje</label>
+                <textarea name="message" id="message" cols="30" rows="10"></textarea>
+              </div>
+              <div className="input-field send">
+                <input type="submit" value="Enviar"/>
+              </div>
+            </form>
+          )
+        }
+        {
+          !isAuthenticated() && (
+            <div className="container-button">
+              <p>Para enviar un mensaje debes estar logueado</p>
+              <a href="#!" onClick={this.youArelogin} className="button">Iniciar Sesión</a>
+            </div>
+          )
+        }
+      </Fragment>
+    );
+  }
+}
 
 export default Contact;
