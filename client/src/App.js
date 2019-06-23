a41669120746052bc38a699432211e1f58c0efbb
CesarRivasP <cesarjose58@gmail.com>
2019-06-23 07:59:53 -0400

    others changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
-import Router from './components/router';
+// import Router from './components/router';
 import { makeMainRoutes } from './components/routes';
+
 
 const routes = makeMainRoutes();
 
 function App() {
   return (
     <Fragment>
-      // <Router />
       { routes }
     </Fragment>
   );
